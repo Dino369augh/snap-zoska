86662abc64cacbcb12579cb1d126f4c8e9196229
Dino369augh <duofifo123@gmail.com>
2024-09-26 10:40:46 +0200

    core-structure-working

---
+++ src/app/gdpr/page.tsx
@@ -0,0 +1,14 @@
+//src/app/gdpr/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: "GDPR | ZoškaSnap"};
+
+export default function Gdpr() {
+  return (
+
+      <Typography> GDPR </Typography>
+    
+  );
+
+}
---
+++ src/app/pridat/page.tsx
@@ -0,0 +1,14 @@
+//src/app/pridat/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: "Formulár na pridanie | ZoškaSnap"};
+
+export default function FormularAdd() {
+  return (
+
+      <Typography> Formulár na pridanie </Typography>
+    
+  );
+
+}
